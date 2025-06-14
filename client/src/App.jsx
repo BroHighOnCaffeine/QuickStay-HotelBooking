e6b3847b105867fbb8098a5cd1c1ec5359f1f699
Harshitect <saoharshit05@gmail.com>
2025-06-15 02:43:29 +0530

    hidding Navbar from Owner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+  // The Navbar will be displayed on all pages but is needed to be hidden from Owner 
+
+  // finding pathname of Owner[Admin]
+  const isOwnerPath = useLocation().pathname.includes("owner"); //This will return a Boolean
+  // for value = True we'll hide Navbar.
+
+ 
+
+
   return (
     <div>
-      <Navbar/>
+      {!isOwnerPath && <Navbar/>}  
+      {/* when ever we will be on Owner Path then this Navbar will be hidden */}
     </div>
   )
 }
