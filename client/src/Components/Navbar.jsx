14852f7a96842d696a4c0fbe22e0e34c1139efae
Harshitect <saoharshit05@gmail.com>
2025-06-16 03:30:11 +0530

    hiding Login Button, display user profile

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { assets } from "../assets/assets";
+import { useClerk, useUser, UserButton } from '@clerk/clerk-react';
 
+
+
+const BookIcon = ()=>(
+    <svg className="w-4 h-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
+</svg>
+)
 
 // this will be a Sticky Navbar with Scroll Effect
 
@@ -16,7 +24,12 @@ const Navbar = () => {
 
 
     const [isScrolled, setIsScrolled] = React.useState(false);
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false); 
+
+    const{openSignIn} = useClerk();
+    const{user} = useUser() ;
+    const navigate = useNavigate() ;
+    const location = useLocation() ;
 
     React.useEffect(() => {
         const handleScroll = () => {
@@ -50,12 +63,23 @@ const Navbar = () => {
                     </button>
                 </div>
 
-                {/* Desktop Right */}
+                {/* Desktop Right  */}
                 <div className="hidden md:flex items-center gap-4">
                     <img src={assets.searchIcon} alt="search" className={`${isScrolled && 'invert'} h-7 transition-all duration-500`} />
-                    <button className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
+                    
+                    {/* USing Ternary Operator , So when user LogSIn the signin Button gets hid and his profile displays */}
+                    {user ? (<UserButton>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label="My Bookings" labelIcon={<BookIcon/>} onClick={()=>navigate('/my-bookings')} />
+                        </UserButton.MenuItems>
+                    </UserButton>)
+                        :
+                        ( <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
                         Login
-                    </button>
+                    </button>)
+                        }
+
+
                 </div>
 {/* This will open navigation drawer since the page is responsive. */}
                 {/*Menu Button */} 
@@ -80,7 +104,7 @@ const Navbar = () => {
                         DashBoard
                     </button>
 
-                    <button className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                    <button  onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
                     </button>
                 </div>
