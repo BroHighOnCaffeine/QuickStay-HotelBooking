5a65a30708fa44bcde6da89e0ce5a4a9b6f43cb5
Harshitect <saoharshit05@gmail.com>
2025-06-17 05:24:55 +0530

    Adding a button in FeaturedDestination Component

--- client/src/Components/FeaturedDestinations.jsx
+++ client/src/Components/FeaturedDestinations.jsx
@@ -4,8 +4,12 @@ import React from "react";
 import { roomsDummyData } from "../assets/assets";
 import HotelCard from "./HotelCard";
 import Title from "./Title";
+import { useNavigate } from "react-router-dom";
 
 const FeaturedDestinations = () => {
+
+    const navigate = useNavigate() ;
+
   return (
     //                                                         lg:px-15 is used for spacing .
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-15 bg-slate-50 py-20'> 
@@ -22,6 +26,14 @@ const FeaturedDestinations = () => {
           <HotelCard key={room._id} room={room} index={index} />
         ))}
       </div>
+
+{/* Adding here ScrollTo(0,0) So it can scroll the webpage to the Top. */}
+      <button onClick={()=>{navigate('/rooms'); scrollTo(0,0)}}
+      className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>
+        
+        View All Destinations
+
+      </button>
     </div>
   );
 };
