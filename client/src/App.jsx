cd9113b53cb6dff43048300afa341839e31042e2
Harshitect <saoharshit05@gmail.com>
2025-06-17 14:20:52 +0530

    New Component - Footer - Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,32 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/Home';
+import React from "react";
+import Navbar from "./Components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./Components/Footer";
 
 const App = () => {
-  // The Navbar will be displayed on all pages but is needed to be hidden from Owner 
+  // The Navbar will be displayed on all pages but is needed to be hidden from Owner
 
   // finding pathname of Owner[Admin]
   const isOwnerPath = useLocation().pathname.includes("owner"); //This will return a Boolean
   // for value = True we'll hide Navbar.
 
-//  Now we've to create the routing for Home Page.
-
+  //  Now we've to create the routing for Home Page.
 
   return (
     <div>
-
       {/* when ever we will be on Owner Path then this Navbar will be hidden */}
-      {!isOwnerPath && <Navbar/>}  
+      {!isOwnerPath && <Navbar />}
 
-
-
-
-
-
-        <div className='min-h-[70vh]'>
-          {/* within this div we've to create Route ; first import routes */}
-          <Routes>
-              <Route path='/' element={<Home/>} />
-          </Routes>
-
-          
-        </div>
+      <div className="min-h-[70vh]">
+        {/* within this div we've to create Route ; first import routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
