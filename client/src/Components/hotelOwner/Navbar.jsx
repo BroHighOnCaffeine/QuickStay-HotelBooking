d086e4c08fe1d15fdeb96ffa532fde8c7628c74c
Harshitect <saoharshit05@gmail.com>
2025-06-22 17:08:13 +0530

    Files for Admin - Initial

---
+++ client/src/Components/hotelOwner/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+import { UserButton } from '@clerk/clerk-react'
+
+const Navbar = () => {
+  return (
+  
+  )
+}
+
+export default Navbar
