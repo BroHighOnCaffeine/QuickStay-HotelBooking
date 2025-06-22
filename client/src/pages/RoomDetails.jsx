b8ab4cf138b7280a010150a0d3228b4fed596727
Harshitect <saoharshit05@gmail.com>
2025-06-22 10:44:56 +0530

    adding Guests description under-  Common-Specifications-section

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { assets, facilityIcons, roomCommonData, roomsDummyData } from "../assets/assets";
+import {
+  assets,
+  facilityIcons,
+  roomCommonData,
+  roomsDummyData,
+} from "../assets/assets";
 import { useState } from "react";
 import StarRating from "../Components/StarRating";
 
@@ -104,57 +109,98 @@ const RoomDetails = () => {
         </div>
 
         {/* CheckIn CheckOut From */}
-        <form className="flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl"> 
-
+        <form className="flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl">
           {/* Input Fields [on the left side] */}
-          <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500'>
+          <div className="flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500">
             {/* First Column */}
             <div className="flex flex-col">
-              <label htmlFor="checkInDate" className="font-medium">Check-In</label>
-              <input type="date" id="checkInDate" placeholder="Check-In" className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
-            </div>
-
-              {/* adding vertical lines between these diff. div */}
-            <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
-
-            {/* Second Column */}
-             <div className="flex flex-col">
-              <label htmlFor="checkOutDate" className="font-medium">Check-Out</label>
-              <input type="date" id="checkOutDate" placeholder="Check-Out" className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+              <label htmlFor="checkInDate" className="font-medium">
+                Check-In
+              </label>
+              <input
+                type="date"
+                id="checkInDate"
+                placeholder="Check-In"
+                className="w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none"
+                required
+              />
             </div>
 
             {/* adding vertical lines between these diff. div */}
-            <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
+            <div className="w-px h-15 bg-gray-300/70 max-md:hidden"></div>
 
-            {/* Third Column */}
-             <div className="flex flex-col">
-              <label htmlFor="guests" className="font-medium">Guests</label>
-              <input type="number" id="guests" placeholder="0" 
-              className="max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none" required/>
+            {/* Second Column */}
+            <div className="flex flex-col">
+              <label htmlFor="checkOutDate" className="font-medium">
+                Check-Out
+              </label>
+              <input
+                type="date"
+                id="checkOutDate"
+                placeholder="Check-Out"
+                className="w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none"
+                required
+              />
             </div>
 
+            {/* adding vertical lines between these diff. div */}
+            <div className="w-px h-15 bg-gray-300/70 max-md:hidden"></div>
 
+            {/* Third Column */}
+            <div className="flex flex-col">
+              <label htmlFor="guests" className="font-medium">
+                Guests
+              </label>
+              <input
+                type="number"
+                id="guests"
+                placeholder="0"
+                className="max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none"
+                required
+              />
+            </div>
           </div>
 
           {/* buttons [on the rifght side ] */}
-          <button type="submit" className="bg-primary hover:bg-primary-dull active:scale-95 transition-all text-white rounded-md max-md:w-full max-md: mt-6 md: px-25 py-3 md:py-4 text-base cursor-pointer" >Check Availability</button>
+          <button
+            type="submit"
+            className="bg-primary hover:bg-primary-dull active:scale-95 transition-all text-white rounded-md max-md:w-full max-md: mt-6 md: px-25 py-3 md:py-4 text-base cursor-pointer"
+          >
+            Check Availability
+          </button>
         </form>
 
         {/* Common Specifications  - Here we'll get the common data from the assets file*/}
-        <div className="mt-25 space-y-4"> 
-        {roomCommonData.map((spec, index)=>(
-          <div key={index} className="flex items-start gap-2">
-            <img src={spec.icon} alt={'${spec.title}-icon'} className="w-6.5" />
-            {/* {spec.icon} to display each images individually */}
-            <div>
-              <p className="text-base">{spec.title}</p>
-              <p className="text-gray-500">{spec.description}</p>
+        <div className="mt-25 space-y-4">
+          {roomCommonData.map((spec, index) => (
+            <div key={index} className="flex items-start gap-2">
+              <img
+                src={spec.icon}
+                alt={"${spec.title}-icon"}
+                className="w-6.5"
+              />
+              {/* {spec.icon} to display each images individually */}
+              <div>
+                <p className="text-base">{spec.title}</p>
+                <p className="text-gray-500">{spec.description}</p>
+              </div>
             </div>
-          </div> 
-
-        ))}
+          ))}
         </div>
 
+        {/* Normal Description */}
+        <div className="max-w-3xl border-y border-gray-300 mt-15 py-10 text-gray-500">
+          <p>
+            Guests will be allocated on the ground floor according to
+            availability. You get a comfortable Two bedroom apartment has a true
+            city feeling. The price quoted is for two guest, at the guest slot
+            please mark the number of guests to get the exact price for groups.
+            The Guests will be allocated ground floor according to availability.
+            You get the comfortable two bedroom apartment that has a true city
+            feeling.
+            {/* before and after this description we have two horizontal lines because we used [border-y border-gray-300] in the className */}
+          </p>
+        </div>
 
       </div>
     )
