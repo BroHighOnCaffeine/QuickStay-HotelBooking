9c28926a7ebbc300859d71d1712d692d53fd2128
Harshitect <saoharshit05@gmail.com>
2025-06-23 16:52:17 +0530

    Create an app using Express and checking the port no of the server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+
+// Creating an app using Express
+const app = express()
+// Enable Cross origin Resource Sharing
+app.use(cors()) // this will help in connect frontend with the backend
+
+
+//req - request and res - response .
+app.get('/', (req ,res) => res.send("API is working."))
+
+
+// For Port Number
+const PORT = process.env.PORT || 3000 ;
+
+app.listen(PORT, ()=>console.log(`Server running on this port ${PORT}`)) ; 
