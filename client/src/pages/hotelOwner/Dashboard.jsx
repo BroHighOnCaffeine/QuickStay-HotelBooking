e854f28408452aee53741a66a3b0e90bac25e37f
Harshitect <saoharshit05@gmail.com>
2025-06-23 13:00:42 +0530

    Completed page - LIST Room

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -46,7 +46,7 @@ const Dashboard = () => {
       <h2 className='text-xl text-blue-950/70 font-medium mb-5'>Recent Bookings</h2>
 
       {/* Now we'll display a Table but keep it inside div  */}
-          <div className='w-full max-w-3x] text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll'>
+          <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll'>
             <table className='w-full'>
               <thead className='bg-gray-50'>
                 <tr>
