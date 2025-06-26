123147b4d584e2820996e626c28e54defc10aa4a
Harshitect <saoharshit05@gmail.com>
2025-06-26 23:01:30 +0530

    new changes 2

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose"
 const connectDB = async ()=> {
     try{
         mongoose.connection.on('connected',()=>console.log("Database Connected")); 
-        await mongoose.connect(`${process.env.MONGODB_URI}/QuickStay-HotelBooking`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/Quickstay`)
         // Here we have to provide the project's Name inplace of "hotel-booking" .
         //  I've changed here the name from "hotel-booking" to "QuickStay-HotelBooking" 
 
