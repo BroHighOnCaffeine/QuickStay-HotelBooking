29e7ca7e8f8a587a951e22f33cf77f6697242b5a
Harshitect <saoharshit05@gmail.com>
2025-06-27 14:02:13 +0530

    two new files -userController.js and authMiddleware.js  to get the userdata from the data base through api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,11 +3,19 @@
 
 export const getUserData = async (req ,res) => {
     try{
+        const role = req.user.role ;
+
+        // we can get here the searched cities data
+        const recentSearchedCities = req.user.recentSearchedCities;
+        res.json({success : true , role , recentSearchedCities})
 
     }catch(error){
+        res.json({success:false , message: error.message})
 
     }
 
 
 }
+
+// Now using this Controller Function we have to create a route
     
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,1 +1,22 @@
-import User from "../models/User";
+//This function will be executed as a MiddleWare before excuting the controller function 
+// This function will check if thw user is authenticated or not
+// If it's authenticated then it will execute the controller function - [userController]
+// Else it will response with message - user not authenticated
+
+import User from "../models/User.js";
+
+// Middleware to check if the user is authenticated
+export const protect = async (req, res, next) => {
+  const { userID } = req.auth;
+  if (!userID) {
+    res.json({ success: false, message: "not aunthenticated" });
+  }else{
+    const user = await User.findById(userId);
+    // This userId - we are getting it from the request auth
+
+    // After this we have to insert "user" in the request
+    req.user =user;
+    next(); 
+  }
+};
+
