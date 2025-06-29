1efa687e062c45331b9bab4834df49774208575c
Harshitect <saoharshit05@gmail.com>
2025-06-29 19:42:12 +0530

    Creating new Route File - roomRoutes.js

---
+++ server/routes/roomRoutes.js
@@ -0,0 +1,13 @@
+import { createRoom } from "../controllers/roomController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import upload from "../middleware/uploadMiddleware.js";
+import express from "express";
+
+const roomRouter = express.Router();
+
+roomRouter.post('/' , upload.array("images" , 4), protect, createRoom ) // FieldName is images and dur to parameter-4 it will upload max. 4 images
+// upload and protect is a MiddleWare
+// And createRoom is a Controller Function .
+
+
+// Now we need to mount this Route in the Server.js File
