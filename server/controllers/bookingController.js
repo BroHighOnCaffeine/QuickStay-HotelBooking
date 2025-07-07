e254f16b57ae616e6c66618401dd2c44507895c8
Harshitect <saoharshit05@gmail.com>
2025-07-07 21:06:31 +0530

    Api Created for Booing Hotel.

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,5 @@
 import Booking from "../models/Booking.js"
+import Room from "../models/Room.js"
 
 
 //  Function to Check Availability of Room 
@@ -40,3 +41,59 @@ export const checkAvailabilityAPI = async (req, res)=>{
     }
 
 }
+
+
+// Now we need to Create the Booking Function - This is a Controller Function
+
+// API to create a new Booking
+
+//  POST /api/bookings/book
+
+export const createBooking = async (req,res)=>{
+    try {
+        
+        const {room , checkInDate , checkOutDate ,guests} = req.body;
+        const user = req.user._id ;
+
+        // Before booking Check Availablity
+
+        const isAvailable = await checkAvailability({
+            checkInDate,
+            checkOutDate,
+            room,
+        })
+
+        if(!isAvailable){
+            return res.json({success : false , message : "Room is not Available"})
+        }
+        // Get Total Price For the Room
+        const roomData = await Room.findById(room).populate("hotel");
+        let totalPrice = roomData.pricePerNight; // This is the price for Per Night
+
+        // Calculate total Price Based on Nights.
+        const checkIn = new Date(checkInDate)
+        const checkOut = new Date(checkOutDate)
+
+        const timeDiff = checkOut.getTime() - checkIn.getTime() ;
+        const nights = Math.ceil(timeDiff / (1000 * 3600 * 24)) ; // Number of nights
+        totalPrice *= nights ;
+
+        const booking = await Booking.create({
+            user,
+            room,
+            hotel : roomData.hotel._id ,
+            guests: +guests,        // by adding this Plus sign it will convert the number into the string 
+            checkInDate,
+            checkOutDate,
+            totalPrice,
+
+        }) // This are the new Data that will be Stored in the Data if we create a New Booking
+
+
+        res.json({success : true , message: "Booking Created SuccessFully!"})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success : false , message: "Booking Failed!" }) ;
+    }
+    }
