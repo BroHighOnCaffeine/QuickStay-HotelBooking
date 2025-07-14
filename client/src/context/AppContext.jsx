14d09963447e1f84af41c0c96f5fc5de14f50699
Harshitect <saoharshit05@gmail.com>
2025-07-15 04:51:34 +0530

    Created Some Variables and StateVariable which'll be used in the Whole WebApp - AppContext.jsx file

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,7 +1,9 @@
 // In this Context Folder we will store all the Central LOGIC , State Variable and Functions That will be used in Entire Web Application
 
 import axios from "axios" ;
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate, useNavigation } from "react-router-dom";
+import { useUser, useAuth } from "@clerk/clerk-react";
 
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 // Now we've BackEnd Url in FrontEnd and we can the Backend url from any file as we've it in AppContext.jsx file
@@ -12,7 +14,18 @@ const AppContext = createContext();
 
 // Creating AppContext Provider Function
 export const AppProvider = ({ children }) => {
+
+    // Creating Some variable and functions that will be used in the whole WebApp
+    const currency = import.meta.env.VITE_CURRENCY ||  "$"  ;   //  || - OR Operator
+    const navigate = useNavigate();
+    const {user} = useUser();
+    const { getToken } = useAuth();
+
+
+    // Creating Some State Variables
     
+    const [isOwner, setIsOwner] = useState(false)
+    const [showHotelReg, setShowHotelReg] = useState(false)
 
         // object
         const value ={
