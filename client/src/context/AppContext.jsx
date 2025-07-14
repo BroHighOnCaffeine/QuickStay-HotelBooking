7836480e44cd7797c37289c8c78d40dd8e3ae606
Harshitect <saoharshit05@gmail.com>
2025-07-15 03:08:41 +0530

    Actually Starting Connecting FrontEnd with BackEnd , Starting from AppContext.jsx file

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,2 +1,29 @@
 // In this Context Folder we will store all the Central LOGIC , State Variable and Functions That will be used in Entire Web Application
 
+import axios from "axios" ;
+import { createContext, useContext } from "react";
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+// Now we've BackEnd Url in FrontEnd and we can the Backend url from any file as we've it in AppContext.jsx file
+
+
+// Now creating createContextHook
+const AppContext = createContext(); 
+
+// Creating AppContext Provider Function
+export const AppProvider = ({ children }) => {
+    
+
+        // object
+        const value ={
+            
+        }
+
+    return (
+            <AppContext.Provider value={value} >
+                {children}
+            </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => useContext(AppContext);
