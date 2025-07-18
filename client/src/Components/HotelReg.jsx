cde9c455e8f3ee38fc29bdf4e8d548882a5decce
Harshitect <saoharshit05@gmail.com>
2025-07-18 18:09:41 +0530

    Inserting the State Variables in Input Feed of the Form - HotelReg.jsx

--- client/src/Components/HotelReg.jsx
+++ client/src/Components/HotelReg.jsx
@@ -28,7 +28,7 @@ const HotelReg = () => {
                     <label htmlFor="name"  className="font-medium text-gray-500">
                     Hotel Name
                     </label>
-                    <input id='name' type="text" placeholder="Type Here" className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light" required />
+                    <input id='name' onChange={ (e)=>setName(e.target.value) } value={name} type="text" placeholder="Type Here" className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light" required />
                 </div>
 
                 {/* Phone - Contact */}
@@ -36,20 +36,21 @@ const HotelReg = () => {
                     <label htmlFor="contact"  className="font-medium text-gray-500">
                     Phone
                     </label>
-                    <input id='contact' type="text" placeholder="Type Here" className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light" required />
+                    <input id='contact' onChange={ (e)=>setContact(e.target.value) } value={contact} type="text" placeholder="Type Here" className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light" required />
                 </div>
+
                  {/* Hotel Address */}
                 <div className='w-full mt-4'>
                     <label htmlFor="address"  className="font-medium text-gray-500">
                     Address
                     </label>
-                    <input id='address' type="text" placeholder="Type Here" className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light" required />
+                    <input id='address' onChange={ (e)=>setAddress(e.target.value) } value={address} type="text" placeholder="Type Here" className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light" required />
                 </div>
 
                 {/* Select City DropDown */}
                 <div className='w-full mt-4 max-w-60 mr-auto'>
                     <label htmlFor="city" className='font-medium text-gray-500'>City</label>
-                    <select id="city" className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required>
+                    <select id="city" onChange={ (e)=>setCity(e.target.value) } value={city} className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required>
                         <option value="">Select City</option>
                         {cities.map((city)=>(
                             <option key={city} value={city}>{city}</option>
@@ -71,4 +72,5 @@ export default HotelReg
 
 
 // Here we'll make API Call using that we can REGISTER the Hotel
-// Before that we need to store the form data using the STATE VARIABLES
+// Before that we need to store the form data using the STATE VARIABLES 
+// & we'll Insert these State Var. in the Input Feed (i.e. input tag)
