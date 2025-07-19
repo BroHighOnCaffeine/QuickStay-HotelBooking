833d4e16c6693214a98b2596d1c9b590cbb4d251
Harshitect <saoharshit05@gmail.com>
2025-07-19 23:35:31 +0530

    Resolved Typo in usedID to userId for Authentication ; Handeler Function Made , is working- HotelReg.jsx

--- client/src/Components/HotelReg.jsx
+++ client/src/Components/HotelReg.jsx
@@ -1,104 +1,161 @@
-import React, { useState } from 'react'
-import { assets, cities } from '../assets/assets'
-import { useAppContext } from '../context/AppContext'
-import toast from 'react-hot-toast';
+import React, { useState } from "react";
+import { assets, cities } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 const HotelReg = () => {
+  const { setShowHotelReg, axios, getToken, setIsOwner } = useAppContext();
 
-    const {setShowHotelReg, axios, getToken, setIsOwner} = useAppContext();
+  // State Variables to Get the Form Data
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [contact, setContact] = useState("");
+  const [city, setCity] = useState("");
 
-    // State Variables to Get the Form Data
-    const[name, setName] = useState("")
-    const[address, setAddress] = useState("")
-    const[contact, setContact] = useState("")
-    const[city, setCity] = useState("")
+  // Creating the Handler Function
+  const onSubmitHandler = async (event) => {
+    try {
+      // Here we Call the API to Register the Hotel
+      event.preventDefault(); // This will Stop the Page from reloading whenever we are filling the Form
+      const token = await getToken();
+      console.log("Token:", token);
 
-    // Creating the Handler Function
-    const onSubmitHandler = async (event)=>{
-        try {
-            // Here we Call the API to Register the Hotel
-            event.preventDefault(); // This will Stop the Page from reloading whenever we are filling the Form
-            const {data} = await axios.post(`/api/hotels/`, {name, contact, address, city} ,{headers: {Authorization : `Bearer ${await getToken()}` }} ) //API call  ;and Api response we'll get the data
-            //                                Api endpoint,  data that we have to send , headers 
-
-            // Checking the Data
-            if(data.success){
-                toast.success(data.message) //data.message is the data we're getting from the API
-                setIsOwner(true)
-                // Now we've to Hide the Registration Form
-                setShowHotelReg(false);
-            }else{
-                toast.error(data.message)
-            }
-
-        } catch (error) {
-            toast.error(error.message)
-            
+      const { data } = await axios.post(
+        `/api/hotels/`,
+        { name, contact, address, city },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
+      );
+
+      //   const { data } = await axios.post(
+      //     `/api/hotels/`,
+      //     { name, contact, address, city },
+      //     { headers: { Authorization: `Bearer ${await getToken()}` } }
+      //   );
+      //API call  ;and from Api response we'll get the data
+      //                                Api endpoint,  data that we have to send , headers
+
+      // Checking the Data
+      if (data.success) {
+        toast.success(data.message); //data.message is the data we're getting from the API
+        setIsOwner(true);
+        // Now we've to Hide the Registration Form
+        setShowHotelReg(false);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
   return (
     // due to onClick in this div , if we click anywhere else except Input feild then it will close the Form
-    <div onClick={ ()=> setShowHotelReg(false) } className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70'>
+    <div
+      onClick={() => setShowHotelReg(false)}
+      className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70"
+    >
+      <form
+        onSubmit={onSubmitHandler}
+        onClick={(e) => e.stopPropagation()}
+        className="flex bg-white rounded-xl max-w-4xl max-md:mx-2"
+      >
+        <img
+          src={assets.regImage}
+          alt="reg-image"
+          className="w-1/2 rounded-xl hidden md:block"
+        />
+        <div className="relative flex flex-col items-center md:w-1/2 p-8 md:p-10">
+          {/* Adding close icon  */}{" "}
+          {/* Adding Onclick function in this button and using AppContext here to pass in parameter*/}
+          <img
+            src={assets.closeIcon}
+            alt="close-icon"
+            className="absolute top-4 right-4 h-4 w-4 cursor-pointer"
+            onClick={() => setShowHotelReg(false)}
+          />
+          <p className="text-2xl font-semibold mt-6">Register Your Hotel</p>
+          {/* Hotel Name */}
+          <div className="w-full mt-4">
+            <label htmlFor="name" className="font-medium text-gray-500">
+              Hotel Name
+            </label>
+            <input
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              type="text"
+              placeholder="Type Here"
+              className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
+              required
+            />
+          </div>
+          {/* Phone - Contact */}
+          <div className="w-full mt-4">
+            <label htmlFor="contact" className="font-medium text-gray-500">
+              Phone
+            </label>
+            <input
+              id="contact"
+              onChange={(e) => setContact(e.target.value)}
+              value={contact}
+              type="text"
+              placeholder="Type Here"
+              className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
+              required
+            />
+          </div>
+          {/* Hotel Address */}
+          <div className="w-full mt-4">
+            <label htmlFor="address" className="font-medium text-gray-500">
+              Address
+            </label>
+            <input
+              id="address"
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
+              type="text"
+              placeholder="Type Here"
+              className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
+              required
+            />
+          </div>
+          {/* Select City DropDown */}
+          <div className="w-full mt-4 max-w-60 mr-auto">
+            <label htmlFor="city" className="font-medium text-gray-500">
+              City
+            </label>
+            <select
+              id="city"
+              onChange={(e) => setCity(e.target.value)}
+              value={city}
+              className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
+              required
+            >
+              <option value="">Select City</option>
+              {cities.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button className="bg-indigo-500 hover:bg-indigo-600 transition-all text-white mr-auto px-6 py-2 rounded cursor-pointer mt-6">
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-        <form onSubmit={onSubmitHandler} onClick={ (e)=> e.stopPropagation() } className='flex bg-white rounded-xl max-w-4xl max-md:mx-2'>
-            <img src={assets.regImage} alt="reg-image" className='w-1/2 rounded-xl hidden md:block' />
-            <div className='relative flex flex-col items-center md:w-1/2 p-8 md:p-10'>
-
-            {/* Adding close icon  */}  {/* Adding Onclick function in this button and using AppContext here to pass in parameter*/}
-                <img src={assets.closeIcon} alt="close-icon" className='absolute top-4 right-4 h-4 w-4 cursor-pointer' onClick={ ()=> setShowHotelReg(false) }/> 
-                <p className='text-2xl font-semibold mt-6'>Register Your Hotel</p>
-
-                {/* Hotel Name */}
-                <div className='w-full mt-4'>
-                    <label htmlFor="name"  className="font-medium text-gray-500">
-                    Hotel Name
-                    </label>
-                    <input id='name' onChange={ (e)=>setName(e.target.value) } value={name} type="text" placeholder="Type Here" className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light" required />
-                </div>
-
-                {/* Phone - Contact */}
-                <div className='w-full mt-4'>
-                    <label htmlFor="contact"  className="font-medium text-gray-500">
-                    Phone
-                    </label>
-                    <input id='contact' onChange={ (e)=>setContact(e.target.value) } value={contact} type="text" placeholder="Type Here" className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light" required />
-                </div>
-
-                 {/* Hotel Address */}
-                <div className='w-full mt-4'>
-                    <label htmlFor="address"  className="font-medium text-gray-500">
-                    Address
-                    </label>
-                    <input id='address' onChange={ (e)=>setAddress(e.target.value) } value={address} type="text" placeholder="Type Here" className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light" required />
-                </div>
-
-                {/* Select City DropDown */}
-                <div className='w-full mt-4 max-w-60 mr-auto'>
-                    <label htmlFor="city" className='font-medium text-gray-500'>City</label>
-                    <select id="city" onChange={ (e)=>setCity(e.target.value) } value={city} className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required>
-                        <option value="">Select City</option>
-                        {cities.map((city)=>(
-                            <option key={city} value={city}>{city}</option>
-                        ))}
-                    </select>
-                </div>
-
-                <button className='bg-indigo-500 hover:bg-indigo-600 transition-all text-white mr-auto px-6 py-2 rounded cursor-pointer mt-6'>Register</button>
-
-
-            </div>
-        </form>
-
-</div>
-  )
-}
-
-export default HotelReg
-
+export default HotelReg;
 
 // Here we'll make API Call using that we can REGISTER the Hotel
-// Before that we need to store the form data using the STATE VARIABLES 
+// Before that we need to store the form data using the STATE VARIABLES
 // & we'll Insert these State Var. in the Input Feed (i.e. input tag)
 
 // In the Form Tag we'll add one OnSubmit Function.
