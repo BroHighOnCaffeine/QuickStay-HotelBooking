2ab437db611c728c43b826396c55c8baf50bbbe9
Harshitect <saoharshit05@gmail.com>
2025-07-22 09:08:28 +0530

    Resolving a Typo in roomController.js

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -72,7 +72,7 @@ export const getRooms = async (req, res) => {
 // API to get All Rooms for a Specific Hotel
 export const getOwnerRooms = async (req, res) => {
     try {
-        const hoteData = await Hotel({owner : req.auth.userId}); // Geting Owner id
+        const hotelData = await Hotel.findOne({owner : req.auth.userId}); // Geting Owner id
 
         // Now getting Rooms of that Hotel
         const rooms = await Room.find({hotel : hotelData._id.toString()}).populate("hotel");
