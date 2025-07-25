0b120bb0ec8c03b12aeb36acfe75d8f4d798bdad
Harshitect <saoharshit05@gmail.com>
2025-07-25 19:52:30 +0530

    Calling func. fetchDashboardData whenever page gets loaded & changing the currency Symbol - Dashboard.jsx

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from '../../Components/Title'
 import { assets,  } from '../../assets/assets'
 import {useAppContext} from '../../context/AppContext'
@@ -35,6 +35,14 @@ const Dashboard = () => {
     }
   }
 
+  // fetchDashboardData function Calling whenever the Page gets loaded
+  useEffect( () => {
+    if (user) {  //if user is  available
+      fetchDashboardData()
+      
+    }
+  }, [user])
+
 
   return (
     <div>
@@ -59,7 +67,7 @@ const Dashboard = () => {
         <img src={assets.totalRevenueIcon} alt=""  className='max-sm:hidden h-10'/>
         <div className='flex flex-col sm:ml-4 font-medium'>
             <p className='text-blue-500 text-lg' >Total Revenue</p>
-            <p className='text-neutral-400 text-base' >Rs. {dashboardData.totalRevenue}</p>
+            <p className='text-neutral-400 text-base' >{currency} {dashboardData.totalRevenue}</p>
 
         </div>
 
@@ -96,7 +104,7 @@ const Dashboard = () => {
                       {item.room.roomType}
                     </td>
                     <td className='py-3 px-4 text-gray-700 max-sm:hidden border-t border-gray-300 text-center'>
-                     Rs. {item.totalPrice}
+                     {currency} {item.totalPrice}
                     </td>
                     <td className='py-3 px-4 text-gray-700 border-t border-gray-300 flex'>
                       <button className={`py-1 px-3 text-xs rounded-full mx-auto ${item.isPaid ? "bg-green-200 text-green-600" : "bg-amber-200 text-yellow-600"}`}>{item.isPaid ? "Completed" : "Pending"}</button>
